658884cf0df925b618bf47322c3696f86b6c5f7c
Julio Meza <juliomeza2512@gmail.com>
2025-09-08 20:20:16 -0500

    primera versión funcional

---
+++ src/pruebasql/Pruebasql.java
@@ -0,0 +1,20 @@
+
+package pruebasql;
+import java.sql.*;
+
+
+public class Pruebasql {
+
+   
+    public static void main(String[] args) {
+     
+        CConexion conexion = new CConexion();
+        Connection con= conexion.conectar();
+        CCrud crud = new CCrud(con);
+        crud.consultar();
+        
+        
+        
+    }
+    
+}
